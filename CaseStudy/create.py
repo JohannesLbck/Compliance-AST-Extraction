c68b1218c8229931174b3713afbaf2e222bdffb1
JohannesLbck <loebbeckejohannes@hotmail.de>
2026-03-04 14:54:22 +0100

    added simple file search store create script

---
+++ CaseStudy/create.py
@@ -0,0 +1,9 @@
+from google import genai
+from google.genai import types
+import time
+
+client = genai.Client()
+
+file_search_store = client.file_search_stores.create(config={'display_name': 'bpm26casestudy'})
+
+print(f"Created File Search Store: {file_search_store.name}")
